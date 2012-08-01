763a95a7b7bf866a9347b6bfd99baf9276ecae85
Joel Moore <joelbm24@gmail.commit>
2012-08-01 16:21:21 -0700

    BROKEN: started to parse it into a map

--- parser.go
+++ parser.go
@@ -7,15 +7,17 @@ import (
     "strings"
 )
 
+var parsed_expr map[string] string
+
 func main() {
-    expr := "2333,33m5"
+    expr := "4p"
 
     cmd, err := regexp.Compile("[a-zA-Z]")
     number, err := regexp.Compile("[1-9]*")
     num_range, err := regexp.Compile("[1-9]+,[1-9]+")
     re_expr, err := regexp.Compile("[a-z]/[a-zA-Z]+/")
 
-    var parsed_expr []string
+    parsed_expr = make(map[string]string)
 
     if err != nil {
         log.Fatal(err)
@@ -25,25 +27,26 @@ func main() {
         switch {
             case re_expr.MatchString(expr):
                 tmp := strings.SplitAfter(expr, re_expr.FindString(expr))[0]
-                parsed_expr = append(parsed_expr, tmp)
+                parsed_expr["REGEX"] = tmp
                 if expr != "" { expr = strings.SplitAfter(expr, re_expr.FindString(expr))[1] }
 
             case num_range.MatchString(expr):
                 tmp := strings.SplitAfter(expr, num_range.FindString(expr))[0]
-                parsed_expr = append(parsed_expr, tmp)
+                parsed_expr["RANGE"] = tmp
                 if expr != "" { expr = strings.SplitAfter(expr, num_range.FindString(expr))[1] }
-
-            case number.MatchString(expr):
-                tmp := strings.SplitAfter(expr, number.FindString(expr))[0]
-                parsed_expr = append(parsed_expr, tmp)
-                if expr != "" { expr = strings.SplitAfter(expr, number.FindString(expr))[1] }
 
             case cmd.MatchString(expr):
                 tmp := strings.SplitAfter(expr, cmd.FindString(expr))[0]
-                parsed_expr = append(parsed_expr, tmp)
+                parsed_expr["CMD"] = tmp
                 if expr != "" { expr = strings.SplitAfter(expr, cmd.FindString(expr))[1] }
 
+            case number.MatchString(expr):
+                tmp := strings.SplitAfter(expr, number.FindString(expr))[0]
+                parsed_expr["NUMBER"] = tmp
+                if expr != "" { expr = strings.SplitAfter(expr, number.FindString(expr))[1] }
+
             default: log.Fatal("Invalid") // TODO make it exit better
+
         }
     }
     fmt.Println(parsed_expr)
